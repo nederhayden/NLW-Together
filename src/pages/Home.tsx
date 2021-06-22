877885cd8448dd8e7f2b3d75c362ba255fd76329
Neder <nedersimoes@gmail.com>
2021-06-22 18:18:10 -0400

    firebase authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -11,8 +12,14 @@ import "../styles/auth.scss";
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push("/rooms/new");
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      
+      history.push("/rooms/new");
+    })
+
   }
 
   return (
@@ -28,7 +35,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
