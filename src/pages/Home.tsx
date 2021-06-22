e72a9fccc6f69d9209014ea9e69243365cf4bfef
Neder <nedersimoes@gmail.com>
2021-06-22 19:22:08 -0400

    hooking authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -6,13 +5,13 @@ import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
-import { AuthContext } from "../App";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,15 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
+// import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
-import { AuthContext } from "../App";
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  // const { user } = useAuth();
 
   return (
     <div id="page-auth">
